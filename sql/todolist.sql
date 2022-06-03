7226ec3d328876e5a949ca79acfb43a8a130eec7
Jinyang Zhang <jinyang.zhang@rea-group.com>
2022-06-03 10:28:50 +0800

    change mysql file

--- sql/todolist.sql
+++ sql/todolist.sql
@@ -1,9 +1,13 @@
--- ----------------------------
--- Table structure for `todolist`
--- ----------------------------
-CREATE TABLE IF NOT EXISTS `todolist` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `text` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
-  `complete` tinyint DEFAULT '0',
-  PRIMARY KEY (`id`)
+
+-- -- ----------------------------
+-- -- Table structure for `todolist`
+-- -- ----------------------------
+CREATE TABLE IF NOT EXISTS todolist (
+  id int unsigned NOT NULL AUTO_INCREMENT,
+  text varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  complete tinyint DEFAULT 0,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO todolist (id, text, complete) VALUES
+(1,'learn aws',0);
